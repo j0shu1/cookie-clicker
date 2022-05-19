6c59222e499c4b17fd5cdc262798b7abd3310992
JoshJ-WDD <joh21036@byui.edu>
2022-05-18 18:33:01 -0600

    vast UI improvements & added test bar

--- scripts/main.js
+++ scripts/main.js
@@ -1,30 +1,9 @@
 const cookies = document.getElementById("cookies");
-const cookieImg = document.getElementById("cookieImg");
 let increment = 1;
 let cookieNum = 0;
 
-// Helper functions
-function getCookies() {
-    let originalString = cookies.textContent
-    return parseInt(originalString.substring(0, originalString.length - 7));
-}
-
-// Button functions
-function upgradeClick() {
-    cookieNum -= increment * 30;
-    increment = increment * 2;
-    updateCookiesPerClick();
-    update();
-}
-
-function addCookie() {
-    cookieNum = getCookies() + increment;
-    update();
-}
-
-// Display functions
-
-function setCookies() {
+// Update functions
+function showCookies() {
     cookies.textContent = cookieNum + " cookies";
 }
 
@@ -37,25 +16,50 @@ function updateUpgradeCost() {
 }
 
 function makeUpgradesAvailable() {
-    if (getCookies() >= increment * 30) {
+    if (cookieNum >= increment * 30) {
         document.getElementById("upgradeClick").disabled=false;
     } else {
         document.getElementById("upgradeClick").disabled=true;
     }
 }
 
-function setCookieSize() {
-    if ((cookieNum < 640) || (cookieNum >= 640 && cookieImg.getAttribute("width") < 640)) {
-        cookieImg.setAttribute("width", cookieNum);
-    }
-}
-
 function update() {
-    setCookies();
+    showCookies();
     updateUpgradeCost();
     makeUpgradesAvailable();
     setCookieSize();
 }
 
-document.getElementById("cookieButton").addEventListener("click", addCookie);
+// Button functions
+function upgradeClick() {
+    cookieNum -= increment * 30;
+    increment = increment + 1;
+    updateCookiesPerClick();
+    update();
+}
+
+function addCookie() {
+    cookieNum += increment;
+    update();
+}
+
+document.getElementById("cookieImg").addEventListener("click", addCookie);
 document.getElementById("upgradeClick").addEventListener("click", upgradeClick);
+
+function move() {
+    var elem = document.getElementById("myBar");
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+        if (width >= 500) {
+            clearInterval(id);
+            width = 0;
+            move();
+            addCookie();
+        } else {
+        width += 5;
+        elem.style.width = width + 'px';
+        }
+    }
+}
+move();
